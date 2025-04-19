c8e587db8407ba91df2b125bf487b3ab63b72720
kumsushl <sushiljhu@gmail.com>
2025-04-19 18:23:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+DZ2rnThBDuI1Nm
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002572675300').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sushiljhu:sushiljhu@autofilter.jq40d9p.mongodb.net/?retryWrites=true&w=majority&appName=autofilter")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sushiljhu:uL5odWbuW9UMWf6A@cluster0.0akbfjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "autofilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
